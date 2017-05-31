a550294379cf60df4a24e81931453e583d4bd70b
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-31 15:00:26 -0400

    Doc paragaphs

--- md_to_html.py
+++ md_to_html.py
@@ -2,3 +2,19 @@
 This module is a standalone script to convert Markdown into HTML.
 It reads Markdown from stdin and writes HTML to stdout.
 """
+
+def paragraphs(corpus):
+    """
+    Inserts <p> and </p> tags appropriately into corpus.
+
+    Parameters
+    ----------
+    corpus : str
+        The raw or partially converted Markdown text.
+
+    Returns
+    -------
+    para_corpus : str
+        The corpus after paragraph tags are inserted.
+    """
+    pass

df86f8bc99f9ddcd94190e7f9fb71e1ac4cd7aba
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-31 15:50:58 -0400

    Test paragraphs

--- md_to_html.py
+++ md_to_html.py
@@ -3,8 +3,12 @@ This module is a standalone script to convert Markdown into HTML.
 It reads Markdown from stdin and writes HTML to stdout.
 """
 
+
+import re
+
+
 def paragraphs(corpus):
-    """
+    r"""
     Inserts <p> and </p> tags appropriately into corpus.
 
     Parameters
@@ -14,7 +18,18 @@ def paragraphs(corpus):
 
     Returns
     -------
-    para_corpus : str
+    all_paras_added : str
         The corpus after paragraph tags are inserted.
+
+    >>> text = 'P1\n\nP2\nP2\n    \t   \n\n\n  \t\nP3'
+    >>> paragraphs(text)
+    '<p>P1</p>\n<p>P2\nP2</p>\n<p>P3</p>'
     """
-    pass
+    inner_paras_added = re.sub(r'\n\s*\n', '</p>\n<p>', corpus)
+    all_paras_added = f'<p>{inner_paras_added}</p>'
+    return all_paras_added
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
